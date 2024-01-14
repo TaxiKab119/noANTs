264e3c01a6af288ba09b6c846cf082c60482a761
Alex Balan <cs.k.alexanderbalan@gmail.com>
2024-01-13 23:07:53 -0500

    Finish AddThoughtsScreen

--- app/src/main/java/com/example/thoughtapp/addthought/AddThoughtScreen.kt
+++ app/src/main/java/com/example/thoughtapp/addthought/AddThoughtScreen.kt
@@ -8,17 +8,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -37,6 +38,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -61,31 +63,107 @@ fun AddThoughtsScreen() {
             verticalArrangement = Arrangement.spacedBy(12.dp),
             modifier = Modifier.padding(it)
         ) {
-            items(10) {
+            item {
                 CustomTextField(
                     label = "what was the context leading up to the thought? who, what, where, when and why? (e.g., just got off the phone with my dad and was about to make dinner.)",
                     title = "situation"
                 )
-                CustomTextField(label = "what are you feeling? emotionally? physically? (e.g., angry, sad, butterflies in stomach)", title = "emotion" )
-                CustomSlider(title = "strength of emotion")
-                CustomTextField(label = "what is going through your mind? (e.g., I feel like I am not good enough. Like an impostor.", title = "thought")
-                CustomSlider(title = "belief in thought")
-                Text(
-                    text = "now challenge the negative thought",
-                    style = TextStyle(
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold
-                    ),
-                    modifier = Modifier.padding(start = 16.dp, top = 8.dp)
+                CustomTextField(
+                    label = "what are you feeling? emotionally? physically? (e.g., angry, sad, butterflies in stomach)",
+                    title = "emotion"
                 )
-                CustomTextField(label = "give 1-3 reasons why this thought may be true", title = "may be true because...")
-                CustomTextField(label = "give 1-3 reasons why this thought may NOT be true", title = "may not be true because...")
-                CustomTextField(label = "Given all the evidence, is there a better way of summing up the situation?", title = "reconsider")
+                CustomSlider(title = "strength of emotion")
+                CustomTextField(
+                    label = "what is going through your mind? (e.g., I feel like I am not good enough. Like an impostor.)",
+                    title = "thought"
+                )
+                CustomSlider(title = "belief in thought")
+                Spacer(Modifier.height(48.dp))
+                CustomSectionDivider(title = "challenge the negative thought")
+                Spacer(Modifier.height(12.dp))
+                CustomTextField(
+                    label = "give 1-3 reasons why this thought may be true",
+                    title = "may be true because..."
+                )
+                CustomTextField(
+                    label = "give 1-3 reasons why this thought may NOT be true",
+                    title = "may not be true because..."
+                )
+                CustomTextField(
+                    label = "given all the evidence, is there a better way of summing up the situation?",
+                    title = "reconsider"
+                )
                 CustomSlider(title = "strength of belief in new thought")
+                Spacer(Modifier.height(12.dp))
+                TextButton(
+                    onClick = { /*TODO - onSaveClick*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                ) {
+                    Text(
+                        text = "save",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.secondary
+                        ),
+                        modifier = Modifier
+                            .border(
+                                width = 4.dp,
+                                shape = RectangleShape,
+                                color = MaterialTheme.colorScheme.secondary
+                            )
+                            .padding(12.dp)
+                    )
+                }
             }
+
         }
     }
 }
+
+@Composable
+fun CustomSectionDivider(
+    title: String
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(4.dp)
+                .background(color = MaterialTheme.colorScheme.secondary)
+        )
+        Box(
+            modifier = Modifier
+                .background(
+                    color = MaterialTheme.colorScheme.secondary,
+                    shape = RoundedCornerShape(
+                        topStart = 0.dp, // Square corner
+                        topEnd = 0.dp, // Square corner
+                        bottomEnd = 16.dp, // Rounded corner
+                        bottomStart = 16.dp // Rounded corner
+                    )
+                )
+                .wrapContentWidth(align = Alignment.CenterHorizontally)
+        ) {
+            Text(
+                text = title,
+                style = TextStyle(
+                    color = MaterialTheme.colorScheme.primary,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                ),
+                modifier = Modifier.padding(vertical = 8.dp, horizontal = 12.dp)
+            )
+        }
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomTextField(
@@ -141,7 +219,7 @@ fun CustomSlider(
     modifier: Modifier = Modifier,
     title: String,
     onValueChange: (Int) -> Unit = {},
-    initialSliderPosition: Float = 0.49f
+    initialSliderPosition: Float = 0f
 ) {
     var sliderPosition by remember { mutableFloatStateOf(initialSliderPosition) }
     Column(
@@ -153,7 +231,9 @@ fun CustomSlider(
                 fontSize = 18.sp,
                 fontWeight = FontWeight.Bold
             ),
-            modifier = Modifier.padding(start = 16.dp, top = 8.dp)
+            modifier = Modifier
+                .padding(start = 16.dp, top = 8.dp)
+                .align(Alignment.Start)
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(text = "${(sliderPosition * 100).toInt() + 1}")
